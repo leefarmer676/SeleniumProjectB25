f74ae49679c4ae843f21bbfd0a4425fff33dcc5d
leefarmer676 <lee.farmer0208@gmail.com>
2022-02-10 12:49:33 -0700

    First locator method - link text

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T4_LibraryLoginPage.java
@@ -0,0 +1,32 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T4_LibraryLoginPage {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("http://library2.cybertekschool.com/login.html");
+
+        //enter username
+        WebElement usernameInput = driver.findElement(By.className("form-control"));
+        usernameInput.sendKeys("incorrect@email.com");
+        //enter password
+        WebElement passwordInput = driver.findElement(By.id("inputPassword"));
+        passwordInput.sendKeys("incorrect password");
+
+        //click to sign in button
+        WebElement signInButton = driver.findElement(By.tagName("button"));
+        signInButton.click();
+        //verify visually error message
+    }
+}
