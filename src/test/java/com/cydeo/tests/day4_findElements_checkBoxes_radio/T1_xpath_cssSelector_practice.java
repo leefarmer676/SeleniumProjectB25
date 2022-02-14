cef2bd294fb1fe4506cf1756778f7b6ad015f981
leefarmer676 <lee.farmer0208@gmail.com>
2022-02-13 20:04:53 -0700

    Day 4 T1 completed

--- src/test/java/com/cydeo/tests/day4_findElements_checkBoxes_radio/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkBoxes_radio/T1_xpath_cssSelector_practice.java
@@ -25,10 +25,27 @@ public class T1_xpath_cssSelector_practice {
         WebElement header_2 = driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
 
         //c. "e-mail" text
+        WebElement emailLabel = driver.findElement(By.xpath("//label[@for='email']"));
+
         //d. email input box
-        //e. "retrieve passowrd button
+        WebElement emailBox = driver.findElement(By.xpath("//input[@name='email']"));
+        //by using xpath contains method:
+        WebElement emailLabel1 = driver.findElement(By.xpath("//input[contains(@pattern,'0-9')]"));
+
+        //e. "retrieve password button
+        WebElement passwordRetrieve1 = driver.findElement(By.xpath("//button[@id='form_submit']"));
+
         //f. "powered by cydeo text
+        WebElement poweredByCydeoText = driver.findElement(By.xpath("//div[@style='text-align: center;']"));
 
         //4 verify all web elements
+        System.out.println("homeLink_1.isDisplayed() = " + homeLink_1.isDisplayed());
+        System.out.println("header_1.isDisplayed() = " + header_1.isDisplayed()); //header_1.isDisplayed().soutv
+        System.out.println("emailLabel.isDisplayed() = " + emailLabel.isDisplayed());
+        System.out.println("emailBox.isDisplayed() = " + emailBox.isDisplayed());
+        System.out.println("passwordRetrieve1.isDisplayed() = " + passwordRetrieve1.isDisplayed());
+        System.out.println("poweredByCydeoText.isDisplayed() = " + poweredByCydeoText.isDisplayed());
+
+
     }
 }
