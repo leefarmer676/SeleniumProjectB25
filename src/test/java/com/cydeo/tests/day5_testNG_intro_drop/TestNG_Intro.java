c268ccf4066e5cf34e25e658d3fdb5da21bf6d28
leefarmer676 <lee.farmer0208@gmail.com>
2022-02-15 17:33:14 -0700

    day 5 TestNG_Intro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_drop/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_drop/TestNG_Intro.java
@@ -1,14 +1,38 @@
 package com.cydeo.tests.day5_testNG_intro_drop;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
 
-    @Test //adding this made it runnable
-    public void test1(){
-        System.out.println("Test 1 is running...");
-
-
-
+    @BeforeMethod //this will run as many times as the number of test being run
+    public void setupMethod(){
+        System.out.println("BeforeMethod is running");
     }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("afterMethod is running");
+    }
+
+
+    @Test (priority = 2)//adding this made it runnable
+    public void test1() {
+        System.out.println("Test 1 is running...");
+    }
+    @Test (priority = 1)
+    public void test2(){
+            System.out.println("Test 2 is running...");
+        }
+
+        @BeforeClass
+        public void setUpMethod(){
+            System.out.println("beforeClass is running");
+        }
+
+        @AfterClass
+        public void tearDownClass(){
+            System.out.println("AfterClass is running");
+        }
+
+
 }
