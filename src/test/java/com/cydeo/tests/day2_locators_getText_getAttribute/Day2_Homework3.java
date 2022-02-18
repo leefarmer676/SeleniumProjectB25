45a4f671416293a2e38fdee54917b105b7d56532
leefarmer676 <lee.farmer0208@gmail.com>
2022-02-18 11:57:13 -0700

    Day2 HW3 completed

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/Day2_Homework3.java
@@ -0,0 +1,34 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Day2_Homework3 {
+    public static void main(String[] args) {
+        WebDriver driver;
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        driver.get("https://google.com");
+        driver.findElement(By.linkText("Gmail")).click();
+
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.contains("Gmail")){
+            System.out.println("title verification passed");
+        }else{
+            System.out.println("title verification failed");
+        }
+        driver.navigate().back();
+
+        String actualHomeTitle = driver.getTitle();
+        String expectedHomeTitle = "Google";
+
+        if(actualHomeTitle.equals(expectedHomeTitle)){
+            System.out.println("homepage title verification passed");
+        }else{
+            System.out.println("homepage title verification failed");
+        }
+    }
+}
